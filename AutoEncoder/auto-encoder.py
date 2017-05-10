f1f2c1986a502346bf579831815edc1360ace258
张智 <zhangzhi@zhangzhideMBP.lan>
2017-05-10 14:58:20 +0800

    update

--- AutoEncoder/auto-encoder.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pickle, gzip
-
-with gzip.open('./mnist.pkl.gz') as fp:
-    training_data, valid_data, test_data = pickle.load(fp,encoding='latin1')
-print(print(training_data[0]))
-
-
